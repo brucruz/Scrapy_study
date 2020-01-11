36dc212ac7fb4608dbca17941d41a5771ad38a2f
Bruno Cruz <bru.cruz@gmail.com>
2020-01-10 21:20:03 -0300

    adicionadas modificações para incluir o padrão dos items

--- noticias/spiders/Tecnoblog.py
+++ noticias/spiders/Tecnoblog.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from noticias.items import NoticiasItem
 
 
 class TecnoblogSpider(scrapy.Spider):
@@ -8,4 +9,10 @@ class TecnoblogSpider(scrapy.Spider):
     start_urls = ['http://tecnoblog.net/']
 
     def parse(self, response):
-        pass
+        for article in response.css("article"):
+          link    = article.css("div.texts h2 a::attr(href)").extract_first()
+          title   = article.css("div.texts h2 a::text").extract_first()
+          author  = article.css("div.texts div.info a::text").extract_first()
+
+          notice = NoticiasItem(title=title, author=author, link=link)
+          yield notice
